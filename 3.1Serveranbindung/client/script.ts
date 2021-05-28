f96f8f17387feb377b46eaf0f87de998f60ec90e
Marissa99 <reiser-marissa@t-online.de>
2021-05-28 20:47:58 +0200

    3.1 Abgabe_fertig

--- 3.1Serveranbindung/client/script.ts
+++ 3.1Serveranbindung/client/script.ts
@@ -1,24 +1,24 @@
 namespace P_3_1Server {
     //Teilaufgabe 2
-    async function sendData(): Promise<void> {
+    async function sendData(): Promise<void> { //async Funktion
         let formData: FormData = new FormData (document.forms[0]);
-        console.log(":" + formData.get ("name")); //Ausgabe
+        console.log(":" + formData.get ("name")); //Konsolenausgabe
         for (let entry of formData) {
             console.log(entry);
-            console.log("name: " + entry[0]); //Ausgabe
-            console.log("name: " + entry[1]); //Ausgabe
+            console.log("name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("name: " + entry[1]); //Konsolenausgabe
         }
         let  query: URLSearchParams = new URLSearchParams(<any> formData);
-        let _url: RequestInfo = "https://gissose2021mr.herokuapp.com/";
-        _url = _url + "?" + query.toString();
-        console.log(_url);
+        let _url: RequestInfo = "https://gissose2021mr.herokuapp.com/"; //Verknüpfung mit der herokuapp
+        _url = _url + "?" + query.toString(); //Url in String umwandeln
+        console.log(_url); //Konsolenausgabe
         let response: Response = await fetch (_url); // auf url warten
-        let antwort: string = await response.text();
+        let antwort: string = await response.text(); //Auf die antwort warten
         console.log (antwort); 
-        //let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverausgabe");
-        //rueckgabe.innerText = antwort;
+        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverausgabe"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        rueckgabe.innerText = antwort; 
     }
-    let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("button");
-    button.addEventListener("click", sendData);
+    let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("button"); 
+    button.addEventListener("click", sendData); //Button um Funktion aufzurufen
   
 }//Ende namespace
--- 3.1Serveranbindung/server/server.js
+++ 3.1Serveranbindung/server/server.js
@@ -8,20 +8,20 @@ var P_3_1Server;
     let port = Number(process.env.PORT); //dem Port die Number "Hafen" geben
     if (!port)
         port = 8100; //Port wird auf 8100 gesetzt
-    let server = Http.createServer(); //Port wird erstellt
-    server.addListener("request", handleRequest); //Listener 
-    server.addListener("listening", handleListen); //Listener
-    server.listen(port);
+    let server = Http.createServer(); //Server/Port wird erstellt
+    server.addListener("request", handleRequest); //Listener und Funktion handleRequest wird aufgerufen
+    server.addListener("listening", handleListen); //Listener und Funktion handleListen wird aufgerufen
+    server.listen(port); //Server hört und reagiert auf den Port und startet
     function handleListen() {
-        console.log("Listening"); //Konsolenausgabe
+        console.log("Listening"); //Konsolenausgabe Listening wenn funktion aufgerufen wird
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!"); //Konsolenausgabe
-        console.log(_request.url); //Serverausgabe
+        console.log(_request.url); //Serverausgabe Terminal
         _response.setHeader("content-type", "text/html; charset=utf-8"); //Eigenschaften von HTML
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Zugriffserlaubnis: * alle dürfen darauf zugreifen
-        _response.write(_request.url); // Antwortwas zurückgegeben wird 
-        _response.end(); //Antwort fertig und zurückgeschickt
+        _response.write(_request.url); // Antwort was zurückgegeben wird -->URl
+        _response.end(); //Antwort fertig und zurückgeschickt -->beendet
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=server.js.map
